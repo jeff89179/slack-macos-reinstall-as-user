30fda2a1a368ee5d582bfdd11a51d6eba33187ed
Jeff <jeff89179@gmail.com>
2022-12-05 13:20:58 -0600

    Create slack-reinstall-terminal.sh

---
+++ slack-reinstall-terminal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Find the current console user
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+# Get the UID of the current console user
+uid=$(id -u "$loggedInUser")
+
+
+# Download the latest version of Slack (Universal architecture - x86_x64 / arm64) to /Users/Shared/
+curl -o /Users/Shared/slack-latest.dmg "https://slack.com/api/desktop.latestRelease?arch=universal&variant=dmg&redirect=true" -L
+
+# Silently Mount the DMG
+hdiutil attach /Users/Shared/slack-desktop-latest.dmg -nobrowse
+
+# Copy Slack to the current console user's ~/Applications folder instead of the /Applications folder.
+launchctl asuser $uid mkdir -p /Users/$loggedInUser/Applications
+launchctl asuser $uid mkdir -p cp -r /Volumes/Slack/Slack.app /Users/$loggedInUser/Applications/
+
+# Make sure Slack is not running before you remove the /Applications/Slack.app, then remove it.
+killall Slack
+rm -rf /Applications/Slack.app
+
+# Unmount the Slack DMG
+hdiutil detach -force /Volumes/Slack
+
+# Remove the Slack DMG
+rm /Users/Shared/slack-latest.dmg
