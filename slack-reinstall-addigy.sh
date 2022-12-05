7ac24906918b5f50ba4017dd6048d330f425b812
Jeff <jeff89179@gmail.com>
2022-12-05 13:17:36 -0600

    Create slack-reinstall-addigy.sh

---
+++ slack-reinstall-addigy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Find the current console user
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+# Download the latest version of Slack (Universal architecture - x86_x64 / arm64) to /Users/Shared/
+curl -o /Users/Shared/slack-latest.dmg "https://slack.com/api/desktop.latestRelease?arch=universal&variant=dmg&redirect=true" -L
+
+# Silently Mount the DMG
+hdiutil attach /Users/Shared/slack-desktop-latest.dmg -nobrowse
+
+# Copy Slack to the current console user's ~/Applications folder instead of the /Applications folder.
+/Library/Addigy/user-job -user "$loggedInUser" -run mkdir -p /Users/$loggedInUser/Applications
+/Library/Addigy/user-job -user "$loggedInUser" -run cp -r /Volumes/Slack/Slack.app /Users/$loggedInUser/Applications/ 
+
+# Make sure Slack is not running before you remove the /Applications/Slack.app, then remove it.
+killall Slack
+rm -rf /Applications/Slack.app
+
+# Unmount the Slack DMG
+hdiutil detach -force /Volumes/Slack
+
+# Remove the Slack DMG
+rm /Users/Shared/slack-latest.dmg
